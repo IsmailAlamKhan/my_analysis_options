a5793795727b06172d6c7f0257805390c9cc71bd
IsmailAlamKhan <67656229+IsmailAlamKhan@users.noreply.github.com>
2021-12-12 15:35:43 +0600

    Initial commit

---
+++ example/my_analysis_options_example.dart
@@ -0,0 +1,6 @@
+import 'package:my_analysis_options/my_analysis_options.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/my_analysis_options.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library my_analysis_options;
+
+export 'src/my_analysis_options_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
