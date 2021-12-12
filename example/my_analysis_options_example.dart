e4692af277d9673afd8d8ffdbe2694cde9ed3e56
IsmailAlamKhan <67656229+IsmailAlamKhan@users.noreply.github.com>
2021-12-12 15:36:29 +0600

    removed useless stuff

--- example/my_analysis_options_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:my_analysis_options/my_analysis_options.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
--- lib/my_analysis_options.dart
+++
@@ -1,8 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library my_analysis_options;
-
-export 'src/my_analysis_options_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
